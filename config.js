40f8dbe90582eced13090fb8c7fed6dfebdeedc9
romyedithor <131989709+romyedithor@users.noreply.github.com>
2023-04-27 12:09:56 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCEOGXo70NNQHu9HHM7k18F9Xe-eUc1aSo",
+  authDomain: "storyapp-2adc5.firebaseapp.com",
+  projectId: "storyapp-2adc5",
+  storageBucket: "storyapp-2adc5.appspot.com",
+  messagingSenderId: "1005189386253",
+  appId: "1:1005189386253:web:42d294f3ee79908959c544"
+};
+
+
